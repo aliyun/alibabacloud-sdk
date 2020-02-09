13f0a64168109554b180e0ada4ad239945005eb4
liaoyustudent <wb-ly617236@alibaba-inc.com>
2020-02-09 12:14:15 +0800

    recodegen facebody csharp

---
+++ facebody-20191230/csharp/core/Models/DetectMaskAdvanceRequest.cs
@@ -0,0 +1,18 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AlibabaCloud.Facebody.Models
+{
+    public class DetectMaskAdvanceRequest : TeaModel {
+        [NameInMap("ImageURLObject")]
+        [Validation(Required=true)]
+        public Stream ImageURLObject { get; set; }
+
+    }
+
+}
