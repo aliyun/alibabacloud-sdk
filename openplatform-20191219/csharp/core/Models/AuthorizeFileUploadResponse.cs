2a8d2b56d61296cad332b65f2a965aaaaafb99e7
liaoyustudent <wb-ly617236@alibaba-inc.com>
2020-03-03 15:05:48 +0800

    csharp openplatform suppoted parser2

--- openplatform-20191219/csharp/core/Models/AuthorizeFileUploadResponse.cs
+++ openplatform-20191219/csharp/core/Models/AuthorizeFileUploadResponse.cs
@@ -9,33 +9,33 @@ using Tea;
 namespace AlibabaCloud.SDK.OpenPlatform.Models
 {
     public class AuthorizeFileUploadResponse : TeaModel {
-        [NameInMap("RequestId")]
-        [Validation(Required=true)]
-        public string RequestId { get; set; }
-
         [NameInMap("AccessKeyId")]
         [Validation(Required=true)]
         public string AccessKeyId { get; set; }
-
-        [NameInMap("EncodedPolicy")]
-        [Validation(Required=true)]
-        public string EncodedPolicy { get; set; }
-
-        [NameInMap("Signature")]
-        [Validation(Required=true)]
-        public string Signature { get; set; }
-
-        [NameInMap("ObjectKey")]
-        [Validation(Required=true)]
-        public string ObjectKey { get; set; }
 
         [NameInMap("Bucket")]
         [Validation(Required=true)]
         public string Bucket { get; set; }
 
+        [NameInMap("EncodedPolicy")]
+        [Validation(Required=true)]
+        public string EncodedPolicy { get; set; }
+
         [NameInMap("Endpoint")]
         [Validation(Required=true)]
         public string Endpoint { get; set; }
+
+        [NameInMap("ObjectKey")]
+        [Validation(Required=true)]
+        public string ObjectKey { get; set; }
+
+        [NameInMap("RequestId")]
+        [Validation(Required=true)]
+        public string RequestId { get; set; }
+
+        [NameInMap("Signature")]
+        [Validation(Required=true)]
+        public string Signature { get; set; }
 
         [NameInMap("UseAccelerate")]
         [Validation(Required=true)]
--- openplatform-20191219/csharp/core/Models/Config.cs
+++ openplatform-20191219/csharp/core/Models/Config.cs
@@ -10,19 +10,19 @@ namespace AlibabaCloud.SDK.OpenPlatform.Models
 {
     public class Config : TeaModel {
         [NameInMap("accessKeyId")]
-        [Validation(Required=true)]
+        [Validation(Required=false)]
         public string AccessKeyId { get; set; }
 
         [NameInMap("accessKeySecret")]
-        [Validation(Required=true)]
+        [Validation(Required=false)]
         public string AccessKeySecret { get; set; }
 
-        [NameInMap("type")]
-        [Validation(Required=true)]
+        [NameInMap("credential type")]
+        [Validation(Required=false)]
         public string Type { get; set; }
 
         [NameInMap("securityToken")]
-        [Validation(Required=true)]
+        [Validation(Required=false)]
         public string SecurityToken { get; set; }
 
         [NameInMap("endpoint")]
@@ -30,60 +30,48 @@ namespace AlibabaCloud.SDK.OpenPlatform.Models
         public string Endpoint { get; set; }
 
         [NameInMap("protocol")]
-        [Validation(Required=true)]
+        [Validation(Required=false)]
         public string Protocol { get; set; }
 
         [NameInMap("regionId")]
         [Validation(Required=true)]
         public string RegionId { get; set; }
 
-        [NameInMap("userAgent")]
-        [Validation(Required=true)]
-        public string UserAgent { get; set; }
-
-        [NameInMap("readTimeout")]
-        [Validation(Required=true)]
+        [NameInMap("read timeout")]
+        [Validation(Required=false)]
         public int? ReadTimeout { get; set; }
 
-        [NameInMap("connectTimeout")]
-        [Validation(Required=true)]
+        [NameInMap("connect timeout")]
+        [Validation(Required=false)]
         public int? ConnectTimeout { get; set; }
 
-        [NameInMap("localAddr")]
-        [Validation(Required=true)]
-        public string LocalAddr { get; set; }
-
-        [NameInMap("httpProxy")]
-        [Validation(Required=true)]
+        [NameInMap("http proxy")]
+        [Validation(Required=false)]
         public string HttpProxy { get; set; }
 
-        [NameInMap("httpsProxy")]
-        [Validation(Required=true)]
+        [NameInMap("https proxy")]
+        [Validation(Required=false)]
         public string HttpsProxy { get; set; }
 
-        [NameInMap("noProxy")]
-        [Validation(Required=true)]
-        public string NoProxy { get; set; }
-
-        [NameInMap("socks5Proxy")]
-        [Validation(Required=true)]
+        [NameInMap("socks5 proxy")]
+        [Validation(Required=false)]
         public string Socks5Proxy { get; set; }
 
-        [NameInMap("socks5NetWork")]
-        [Validation(Required=true)]
+        [NameInMap("socks5 NetWork")]
+        [Validation(Required=false)]
         public string Socks5NetWork { get; set; }
 
+        [NameInMap("no proxy")]
+        [Validation(Required=false)]
+        public string NoProxy { get; set; }
+
+        [NameInMap("userAgent")]
+        [Validation(Required=false)]
+        public string UserAgent { get; set; }
+
         [NameInMap("maxIdleConns")]
-        [Validation(Required=true)]
+        [Validation(Required=false)]
         public int? MaxIdleConns { get; set; }
-
-        [NameInMap("endpointType")]
-        [Validation(Required=true)]
-        public string EndpointType { get; set; }
-
-        [NameInMap("openPlatformEndpoint")]
-        [Validation(Required=true)]
-        public int? OpenPlatformEndpoint { get; set; }
 
     }
 
