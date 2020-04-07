251e5ff797ae16918e1cf4cbc5545a8c10529d14
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2020-04-07 13:50:39 +0800

    Generated dysmsapi-20170525 for PHP

---
+++ dysmsapi-20170525/php/src/Dysmsapi/DeleteSmsTemplateRequest.php
@@ -0,0 +1,92 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace AlibabaCloud\SDK\Dysmsapi\V20170525\Dysmsapi;
+
+use AlibabaCloud\Tea\Model;
+
+class DeleteSmsTemplateRequest extends Model
+{
+    /**
+     * @description appKey
+     *
+     * @var string
+     */
+    public $accessKeyId;
+    /**
+     * @description ownerId
+     *
+     * @var int
+     */
+    public $ownerId;
+    /**
+     * @description resourceOwnerAccount
+     *
+     * @var string
+     */
+    public $resourceOwnerAccount;
+    /**
+     * @description resourceOwnerId
+     *
+     * @var int
+     */
+    public $resourceOwnerId;
+    /**
+     * @description templateCode
+     *
+     * @var string
+     */
+    public $templateCode;
+    protected $_name = [
+        'accessKeyId'          => 'AccessKeyId',
+        'ownerId'              => 'OwnerId',
+        'resourceOwnerAccount' => 'ResourceOwnerAccount',
+        'resourceOwnerId'      => 'ResourceOwnerId',
+        'templateCode'         => 'TemplateCode',
+    ];
+
+    public function validate()
+    {
+        Model::validateRequired('templateCode', $this->templateCode, true);
+    }
+
+    public function toMap()
+    {
+        $res                         = [];
+        $res['AccessKeyId']          = $this->accessKeyId;
+        $res['OwnerId']              = $this->ownerId;
+        $res['ResourceOwnerAccount'] = $this->resourceOwnerAccount;
+        $res['ResourceOwnerId']      = $this->resourceOwnerId;
+        $res['TemplateCode']         = $this->templateCode;
+
+        return $res;
+    }
+
+    /**
+     * @param array $map
+     *
+     * @return DeleteSmsTemplateRequest
+     */
+    public static function fromMap($map = [])
+    {
+        $model = new self();
+        if (isset($map['AccessKeyId'])) {
+            $model->accessKeyId = $map['AccessKeyId'];
+        }
+        if (isset($map['OwnerId'])) {
+            $model->ownerId = $map['OwnerId'];
+        }
+        if (isset($map['ResourceOwnerAccount'])) {
+            $model->resourceOwnerAccount = $map['ResourceOwnerAccount'];
+        }
+        if (isset($map['ResourceOwnerId'])) {
+            $model->resourceOwnerId = $map['ResourceOwnerId'];
+        }
+        if (isset($map['TemplateCode'])) {
+            $model->templateCode = $map['TemplateCode'];
+        }
+
+        return $model;
+    }
+}
---
+++ dysmsapi-20170525/php/src/Dysmsapi/ModifySmsSignRequest/signFileList.php
@@ -0,0 +1,60 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+
+namespace AlibabaCloud\SDK\Dysmsapi\V20170525\Dysmsapi\ModifySmsSignRequest;
+
+use AlibabaCloud\Tea\Model;
+
+class signFileList extends Model
+{
+    /**
+     * @description fileSuffix
+     *
+     * @var string
+     */
+    public $fileSuffix;
+    /**
+     * @description fileContents
+     *
+     * @var string
+     */
+    public $fileContents;
+    protected $_name = [
+        'fileSuffix'   => 'FileSuffix',
+        'fileContents' => 'FileContents',
+    ];
+
+    public function validate()
+    {
+        Model::validateRequired('fileSuffix', $this->fileSuffix, true);
+        Model::validateRequired('fileContents', $this->fileContents, true);
+    }
+
+    public function toMap()
+    {
+        $res                 = [];
+        $res['FileSuffix']   = $this->fileSuffix;
+        $res['FileContents'] = $this->fileContents;
+
+        return $res;
+    }
+
+    /**
+     * @param array $map
+     *
+     * @return signFileList
+     */
+    public static function fromMap($map = [])
+    {
+        $model = new self();
+        if (isset($map['FileSuffix'])) {
+            $model->fileSuffix = $map['FileSuffix'];
+        }
+        if (isset($map['FileContents'])) {
+            $model->fileContents = $map['FileContents'];
+        }
+
+        return $model;
+    }
+}
