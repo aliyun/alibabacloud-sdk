91247bb73b23b0f2ce1c8640a6a7078a0c7e2ca1
liaoyustudent <wb-ly617236@alibaba-inc.com>
2020-04-08 13:53:50 +0800

    csharp supported openplatform-20191219

--- openplatform-20191219/csharp/core/Models/AuthorizeFileUploadResponse.cs
+++ openplatform-20191219/csharp/core/Models/AuthorizeFileUploadResponse.cs
@@ -6,7 +6,7 @@ using System.IO;
 
 using Tea;
 
-namespace AlibabaCloud.SDK.OpenPlatform.Models
+namespace AlibabaCloud.SDK.OpenPlatform20191219.Models
 {
     public class AuthorizeFileUploadResponse : TeaModel {
         [NameInMap("AccessKeyId")]
