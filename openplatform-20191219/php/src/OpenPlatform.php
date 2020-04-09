e459df644f7bf706bb50bb9afdcc9897342d72a3
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2020-04-09 18:13:52 +0800

    Generated by php generator 1.1.6 version

--- openplatform-20191219/php/src/OpenPlatform.php
+++ openplatform-20191219/php/src/OpenPlatform.php
@@ -4,168 +4,21 @@
 
 namespace AlibabaCloud\SDK\OpenPlatform\V20191219;
 
-use AlibabaCloud\Credentials\Credential;
+use AlibabaCloud\Endpoint\Endpoint;
 use AlibabaCloud\SDK\OpenPlatform\V20191219\OpenPlatform\AuthorizeFileUploadRequest;
 use AlibabaCloud\SDK\OpenPlatform\V20191219\OpenPlatform\AuthorizeFileUploadResponse;
-use AlibabaCloud\SDK\OpenPlatform\V20191219\OpenPlatform\Config;
-use AlibabaCloud\Tea\Exception\TeaError;
-use AlibabaCloud\Tea\Exception\TeaUnableRetryError;
 use AlibabaCloud\Tea\Model;
-use AlibabaCloud\Tea\Request;
-use AlibabaCloud\Tea\RpcUtils\RpcUtils;
-use AlibabaCloud\Tea\Tea;
 use AlibabaCloud\Tea\Utils\Utils;
 use AlibabaCloud\Tea\Utils\Utils\RuntimeOptions;
 
 class OpenPlatform
 {
-    private $_endpoint;
-    private $_regionId;
-    private $_protocol;
-    private $_userAgent;
-    private $_readTimeout;
-    private $_connectTimeout;
-    private $_httpProxy;
-    private $_httpsProxy;
-    private $_noProxy;
-    private $_socks5Proxy;
-    private $_socks5NetWork;
-    private $_maxIdleConns;
-    private $_credential;
-
-    public function __construct(Config $config)
+    public function __construct($config)
     {
-        if (Utils::isUnset($config)) {
-            throw new TeaError([
-                'name'    => 'ParameterMissing',
-                'message' => "'config' can not be unset",
-            ]);
-        }
-        if (Utils::_empty($config->endpoint)) {
-            throw new TeaError([
-                'name'    => 'ParameterMissing',
-                'message' => "'config.endpoint' can not be empty",
-            ]);
-        }
-        if (Utils::_empty($config->regionId)) {
-            throw new TeaError([
-                'name'    => 'ParameterMissing',
-                'message' => "'config.regionId' can not be empty",
-            ]);
-        }
-        if (Utils::_empty($config->type)) {
-            $config->type = 'access_key';
-        }
-        $credentialConfig = new \AlibabaCloud\Credentials\Credential\Config([
-            'accessKeyId'     => $config->accessKeyId,
-            'type'            => $config->type,
-            'accessKeySecret' => $config->accessKeySecret,
-            'securityToken'   => $config->securityToken,
-        ]);
-        $this->_credential     = new Credential($credentialConfig);
-        $this->_endpoint       = $config->endpoint;
-        $this->_protocol       = $config->protocol;
-        $this->_regionId       = $config->regionId;
-        $this->_userAgent      = $config->userAgent;
-        $this->_readTimeout    = $config->readTimeout;
-        $this->_connectTimeout = $config->connectTimeout;
-        $this->_httpProxy      = $config->httpProxy;
-        $this->_httpsProxy     = $config->httpsProxy;
-        $this->_noProxy        = $config->noProxy;
-        $this->_socks5Proxy    = $config->socks5Proxy;
-        $this->_socks5NetWork  = $config->socks5NetWork;
-        $this->_maxIdleConns   = $config->maxIdleConns;
-    }
-
-    /**
-     * @param string $action
-     * @param string $protocol
-     * @param string $method
-     * @param object $request
-     *
-     * @throws \Exception
-     *
-     * @return array|object
-     */
-    public function _request($action, $protocol, $method, $request, RuntimeOptions $runtime)
-    {
-        $runtime->validate();
-        $_runtime = [
-            'timeouted'      => 'retry',
-            'readTimeout'    => Utils::defaultNumber($runtime->readTimeout, $this->_readTimeout),
-            'connectTimeout' => Utils::defaultNumber($runtime->connectTimeout, $this->_connectTimeout),
-            'httpProxy'      => Utils::defaultString($runtime->httpProxy, $this->_httpProxy),
-            'httpsProxy'     => Utils::defaultString($runtime->httpsProxy, $this->_httpsProxy),
-            'noProxy'        => Utils::defaultString($runtime->noProxy, $this->_noProxy),
-            'maxIdleConns'   => Utils::defaultNumber($runtime->maxIdleConns, $this->_maxIdleConns),
-            'retry'          => [
-                'retryable'   => $runtime->autoretry,
-                'maxAttempts' => Utils::defaultNumber($runtime->maxAttempts, 2),
-            ],
-            'backoff' => [
-                'policy' => Utils::defaultString($runtime->backoffPolicy, 'no'),
-                'period' => Utils::defaultNumber($runtime->backoffPeriod, 0),
-            ],
-            'ignoreSSL' => $runtime->ignoreSSL,
-        ];
-        $_lastRequest = null;
-        $_now         = time();
-        $_retryTimes  = 0;
-        while (Tea::allowRetry($_runtime['retry'], $_retryTimes, $_now)) {
-            if ($_retryTimes > 0) {
-                $_backoffTime = Tea::getBackoffTime($_runtime['backoff'], $_retryTimes);
-                if ($_backoffTime > 0) {
-                    Tea::sleep($_backoffTime);
-                }
-            }
-            $_retryTimes = $_retryTimes + 1;
-
-            try {
-                $_request           = new Request();
-                $accessKeyId        = $this->getAccessKeyId();
-                $accessKeySecret    = $this->getAccessKeySecret();
-                $_request->protocol = Utils::defaultString($this->_protocol, $protocol);
-                $_request->method   = $method;
-                $_request->pathname = '/';
-                $_request->query    = RpcUtils::query(Tea::merge([
-                    'Action'           => $action,
-                    'Format'           => 'json',
-                    'RegionId'         => $this->_regionId,
-                    'Timestamp'        => RpcUtils::getTimestamp(),
-                    'Version'          => '2019-12-19',
-                    'SignatureMethod'  => 'HMAC-SHA1',
-                    'SignatureVersion' => '1.0',
-                    'SignatureNonce'   => Utils::getNonce(),
-                    'AccessKeyId'      => $accessKeyId,
-                ], $request));
-                $_request->headers = [
-                    'host'       => RpcUtils::getHost('OpenPlatform', $this->_regionId, $this->_endpoint),
-                    'user-agent' => $this->getUserAgent(),
-                ];
-                $_request->query['Signature'] = RpcUtils::getSignature($_request, $accessKeySecret);
-                $_lastRequest                 = $_request;
-                $_response                    = Tea::send($_request, $_runtime);
-                $obj                          = Utils::readAsJSON($_response->body);
-                $body                         = Utils::assertAsMap($obj);
-                if (RpcUtils::hasError($body)) {
-                    throw new TeaError([
-                        'message' => $body['Message'],
-                        'data'    => $body,
-                        'code'    => $body['Code'],
-                    ]);
-                }
-
-                return $body;
-            } catch (\Exception $e) {
-                if (Tea::isRetryable($_runtime['retry'], $_retryTimes)) {
-                    continue;
-                }
-
-                throw $e;
-            }
-        }
-
-        throw new TeaUnableRetryError($_lastRequest);
+        parent::__construct($config);
+        $this->_endpointRule = '';
+        $this->checkConfig($config);
+        $this->_endpoint = $this->getEndpoint($this->_productId, $this->_regionId, $this->_endpointRule, $this->_network, $this->_suffix, $this->_endpointMap, $this->_endpoint);
     }
 
     /**
@@ -175,44 +28,33 @@ class OpenPlatform
      */
     public function authorizeFileUpload(AuthorizeFileUploadRequest $request, RuntimeOptions $runtime)
     {
-        return Model::toModel($this->_request('AuthorizeFileUpload', 'HTTPS', 'GET', $request, $runtime), new AuthorizeFileUploadResponse());
+        Utils::validateModel($request);
+
+        return Model::toModel($this->doRequest('AuthorizeFileUpload', 'HTTPS', 'GET', '2019-12-19', 'AK', $request, null, $runtime), new AuthorizeFileUploadResponse());
     }
 
     /**
+     * @param string $productId
+     * @param string $regionId
+     * @param string $endpointRule
+     * @param string $network
+     * @param string $suffix
+     * @param array  $endpointMap
+     * @param string $endpoint
+     *
      * @throws \Exception
      *
      * @return string
      */
-    public function getUserAgent()
+    public function getEndpoint($productId, $regionId, $endpointRule, $network, $suffix, $endpointMap, $endpoint)
     {
-        return Utils::getUserAgent($this->_userAgent);
-    }
-
-    /**
-     * @throws \Exception
-     *
-     * @return string
-     */
-    public function getAccessKeyId()
-    {
-        if (Utils::isUnset($this->_credential)) {
-            return '';
+        if (!Utils::emptySuffix($endpoint)) {
+            return $endpoint;
+        }
+        if (!Utils::emptySuffix($endpointMap['regionId'])) {
+            return $endpointMap['regionId'];
         }
 
-        return $this->_credential->getAccessKeyId();
-    }
-
-    /**
-     * @throws \Exception
-     *
-     * @return string
-     */
-    public function getAccessKeySecret()
-    {
-        if (Utils::isUnset($this->_credential)) {
-            return '';
-        }
-
-        return $this->_credential->getAccessKeySecret();
+        return Endpoint::getEndpointRules($productId, $regionId, $endpointRule, $network, $suffix);
     }
 }
