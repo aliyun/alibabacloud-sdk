1982526be8be2660e1a186201682e42bfa07e92c
liaoyustudent <wb-ly617236@alibaba-inc.com>
2020-04-22 17:18:53 +0800

    csharp supported viapiutils

---
+++ viapiutils-20200401/csharp/core/Models/GetOssStsTokenRequest.cs
@@ -0,0 +1,14 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AlibabaCloud.SDK.Viapiutils20200401.Models
+{
+    public class GetOssStsTokenRequest : TeaModel {
+    }
+
+}
