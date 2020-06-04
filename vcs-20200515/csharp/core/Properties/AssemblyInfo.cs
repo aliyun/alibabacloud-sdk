989ddda0d93964d10ae1b27ac8aa6525cca19bd3
sdk-team <sdk-team@alibabacloud.com>
2020-06-04 10:56:12 +0800

    Update monitor related API.

--- vcs-20200515/csharp/core/Properties/AssemblyInfo.cs
+++ vcs-20200515/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("87f3f800-a60d-11ea-a1f9-03bb67536607")]
+[assembly: Guid("e085cec0-a60e-11ea-b5a6-7ff8a98b21f3")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
