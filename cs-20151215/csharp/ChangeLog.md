9f510e18d18ffccecbb05c9da4f3d7a5661f232b
sdk-team <sdk-team@alibabacloud.com>
2020-06-05 10:31:03 +0800

    Generated csharp 2015-12-15 for CS.

--- cs-20151215/csharp/ChangeLog.md
+++ cs-20151215/csharp/ChangeLog.md
@@ -1,3 +1,3 @@
-2020-06-05 Version: 0.2.0
+2020-06-05 Version: 0.1.2
 - Generated csharp 2015-12-15 for CS.
 
--- cs-20151215/csharp/core/Properties/AssemblyInfo.cs
+++ cs-20151215/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("baed0ff0-a6d3-11ea-9713-31ef12020912")]
+[assembly: Guid("96691c90-a6d4-11ea-b961-d3e94fa714a1")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.1.0")]
-[assembly: AssemblyFileVersion("0.1.1.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
