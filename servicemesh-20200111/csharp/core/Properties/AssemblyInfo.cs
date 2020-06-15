1ccce08fe977e0434e579f23c754eae010281b4a
sdk-team <sdk-team@alibabacloud.com>
2020-06-15 15:10:44 +0800

    Generated csharp 2020-01-11 for servicemesh.

--- servicemesh-20200111/csharp/core/Properties/AssemblyInfo.cs
+++ servicemesh-20200111/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6d1afd50-abbf-11ea-ad31-4bd8b72cb4ef")]
+[assembly: Guid("fbe589a0-aed6-11ea-b03b-b9f85421357b")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
