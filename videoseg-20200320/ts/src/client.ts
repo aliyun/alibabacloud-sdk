ad1cd59401dbf77438b0f1435d4e2a7bce3acc19
sdk-team <sdk-team@alibabacloud.com>
2020-08-13 11:43:53 +0800

    Release SegmentHalfBody.

--- videoseg-20200320/ts/src/client.ts
+++ videoseg-20200320/ts/src/client.ts
@@ -10,6 +10,66 @@ import EndpointUtil from '@alicloud/endpoint-util';
 import { Readable } from 'stream';
 import * as $tea from '@alicloud/tea-typescript';
 
+export class SegmentHalfBodyRequest extends $tea.Model {
+  videoUrl: string;
+  static names(): { [key: string]: string } {
+    return {
+      videoUrl: 'VideoUrl',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      videoUrl: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class SegmentHalfBodyResponse extends $tea.Model {
+  requestId: string;
+  data: SegmentHalfBodyResponseData;
+  static names(): { [key: string]: string } {
+    return {
+      requestId: 'RequestId',
+      data: 'Data',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      requestId: 'string',
+      data: SegmentHalfBodyResponseData,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class SegmentHalfBodyAdvanceRequest extends $tea.Model {
+  videoUrlObject: Readable;
+  static names(): { [key: string]: string } {
+    return {
+      videoUrlObject: 'VideoUrlObject',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      videoUrlObject: 'Readable',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class SegmentVideoBodyRequest extends $tea.Model {
   videoUrl: string;
   static names(): { [key: string]: string } {
@@ -111,6 +171,25 @@ export class GetAsyncJobResultResponse extends $tea.Model {
   }
 }
 
+export class SegmentHalfBodyResponseData extends $tea.Model {
+  videoUrl: string;
+  static names(): { [key: string]: string } {
+    return {
+      videoUrl: 'VideoUrl',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      videoUrl: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class SegmentVideoBodyResponseData extends $tea.Model {
   videoUrl: string;
   static names(): { [key: string]: string } {
@@ -168,9 +247,70 @@ export default class Client extends RPC {
     super(config);
     this._endpointRule = "regional";
     this.checkConfig(config);
-    this._endpoint = this.getEndpoint(this._productId, this._regionId, this._endpointRule, this._network, this._suffix, this._endpointMap, this._endpoint);
+    this._endpoint = this.getEndpoint("videoseg", this._regionId, this._endpointRule, this._network, this._suffix, this._endpointMap, this._endpoint);
   }
 
+
+  async segmentHalfBody(request: SegmentHalfBodyRequest, runtime: $Util.RuntimeOptions): Promise<SegmentHalfBodyResponse> {
+    Util.validateModel(request);
+    return $tea.cast<SegmentHalfBodyResponse>(await this.doRequest("SegmentHalfBody", "HTTPS", "POST", "2020-03-20", "AK", null, $tea.toMap(request), runtime), new SegmentHalfBodyResponse({}));
+  }
+
+  async segmentHalfBodyAdvance(request: SegmentHalfBodyAdvanceRequest, runtime: $Util.RuntimeOptions): Promise<SegmentHalfBodyResponse> {
+    // Step 0: init client
+    let accessKeyId = await this._credential.getAccessKeyId();
+    let accessKeySecret = await this._credential.getAccessKeySecret();
+    let authConfig = new $RPC.Config({
+      accessKeyId: accessKeyId,
+      accessKeySecret: accessKeySecret,
+      type: "access_key",
+      endpoint: "openplatform.aliyuncs.com",
+      protocol: this._protocol,
+      regionId: this._regionId,
+    });
+    let authClient = new OpenPlatform(authConfig);
+    let authRequest = new $OpenPlatform.AuthorizeFileUploadRequest({
+      product: "videoseg",
+      regionId: this._regionId,
+    });
+    let authResponse = await authClient.authorizeFileUploadWithOptions(authRequest, runtime);
+    // Step 1: request OSS api to upload file
+    let ossConfig = new $OSS.Config({
+      accessKeyId: authResponse.accessKeyId,
+      accessKeySecret: accessKeySecret,
+      type: "access_key",
+      endpoint: RPCUtil.getEndpoint(authResponse.endpoint, authResponse.useAccelerate, this._endpointType),
+      protocol: this._protocol,
+      regionId: this._regionId,
+    });
+    let ossClient = new OSS(ossConfig);
+    let fileObj = new $FileForm.FileField({
+      filename: authResponse.objectKey,
+      content: request.videoUrlObject,
+      contentType: "",
+    });
+    let ossHeader = new $OSS.PostObjectRequestHeader({
+      accessKeyId: authResponse.accessKeyId,
+      policy: authResponse.encodedPolicy,
+      signature: authResponse.signature,
+      key: authResponse.objectKey,
+      file: fileObj,
+      successActionStatus: "201",
+    });
+    let uploadRequest = new $OSS.PostObjectRequest({
+      bucketName: authResponse.bucket,
+      header: ossHeader,
+    });
+    let ossRuntime = new $OSSUtil.RuntimeOptions({ });
+    RPCUtil.convert(runtime, ossRuntime);
+    await ossClient.postObject(uploadRequest, ossRuntime);
+    // Step 2: request final api
+    let segmentHalfBodyreq = new SegmentHalfBodyRequest({ });
+    RPCUtil.convert(request, segmentHalfBodyreq);
+    segmentHalfBodyreq.videoUrl = `http://${authResponse.bucket}.${authResponse.endpoint}/${authResponse.objectKey}`;
+    let segmentHalfBodyResp = await this.segmentHalfBody(segmentHalfBodyreq, runtime);
+    return segmentHalfBodyResp;
+  }
 
   async segmentVideoBody(request: SegmentVideoBodyRequest, runtime: $Util.RuntimeOptions): Promise<SegmentVideoBodyResponse> {
     Util.validateModel(request);
@@ -194,7 +334,7 @@ export default class Client extends RPC {
       product: "videoseg",
       regionId: this._regionId,
     });
-    let authResponse = await authClient.authorizeFileUpload(authRequest, runtime);
+    let authResponse = await authClient.authorizeFileUploadWithOptions(authRequest, runtime);
     // Step 1: request OSS api to upload file
     let ossConfig = new $OSS.Config({
       accessKeyId: authResponse.accessKeyId,
